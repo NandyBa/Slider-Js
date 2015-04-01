e12f03a2d95ca044f91e3ec20648ce4ba04256ba
NandyBa <nandyba@yahoo.fr>
2015-04-01 15:15:12 +0200

    Script optimisation

--- js/slider.js
+++ js/slider.js
@@ -2,26 +2,17 @@ jQuery(function(){
 	border_height = 1;
 	$('.slider img:first').addClass('view');
 	$('.slider').each(function(border_height){
-		$this = $(this);
-		$view = $this.children('.view');
+		$slider = $(this);
+		$view = $slider.children('.view');
 		ratio = $view.width() / $view.height();
-		if($view.data('width')){
+		
+		sliderNewSixe = get_sliderNewSixe($view);
+		height = sliderNewSixe.height;
+		width = sliderNewSixe.width;
 
-			width = $view.data('width');
-			height = $view.data('width') / ratio;
+		animate_slider($slider, height, width);
 
-		}else if($view.data('height')){
-			height = $view.data('height');
-			width = ratio * $view.data('height');
-		}else{
-			height = $view.height();
-			width = $view.width();
-		}
-		$this.prepend('<div class="retour"><span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span></div><div class="suivant"><span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span></div>');
-		$this.animate({
-			height : height+border_height*2,
-			width : width+border_height*2
-		})
+		$slider.prepend('<div class="retour"><span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span></div><div class="suivant"><span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span></div>');	
 	})
 	$('.slider .view').show();
 	$('.slider').on('click','.suivant', function(){
@@ -33,69 +24,77 @@ jQuery(function(){
 			$view_next = $slider.children(':nth-child(3)');
 		}
 
-		if($view_next.data('width') || $view_next.data('height')){
-			ratio = $view_next.width() / $view_next.height();
+		sliderNewSixe = get_sliderNewSixe($view_next);
+		slide_height = sliderNewSixe.height;
+		slide_width = sliderNewSixe.width;
 
-			if($view_next.data('width')){
-				slide_width = $view_next.data('width');
-				slide_height = $view_next.data('width') / ratio;
-			}else{
-				slide_height = $view_next.data('height');
-				slide_width = ratio * $view_next.data('height');
-			}
-		}else{
-			$slider.width('100%');
-			var slide_height = $view_next.height();
-			var slide_width = $view_next.width();
-		}
 		$view_next.addClass('view');
 		
 
-		$slider.animate({
-			height : slide_height+border_height*2,
-			width : slide_width+border_height*2
-		})
+		animate_slider($slider, height, width);
 
-		$('.slider .view').show();
-		$view.hide();
-		$view.removeClass('view');
+		change_slide($slider, $view);
 	}),
 	$('.slider').on('click','.retour', function(){
 		$slider = $(this).parent();
 		$view = $slider.children('.view');
 		$view_prev = $view.prev('img');
 
-
 		if($view_prev.length != 1){
 			$view_prev = $slider.children(':last-child');
 		}
-		$view_prev.addClass('view');
-		var slide_height = $view_prev.height();
 
-		if($view_prev.data('width') || $view_prev.data('height')){
-			ratio = $view_prev.width() / $view_prev.height();
+		sliderNewSixe = get_sliderNewSixe($view_prev);
+		slide_height = sliderNewSixe.height;
+		slide_width = sliderNewSixe.width;
 
-			if($view_prev.data('width')){
-				slide_width = $view_prev.data('width');
-				slide_height = $view_prev.data('width') / ratio;
-			}else{
-				slide_height = $view_prev.data('height');
-				slide_width = ratio * $view_prev.data('height');
-			}
+		$view_prev.addClass('view');	
+
+		animate_slider($slider, height, width);
+
+		change_slide($slider, $view);
+	});
+
+	/* Param
+	/	$view is the new image
+	*/
+	function get_sliderNewSixe($view){
+		$slider = $view.parent();
+		if($view.data('width')){
+
+			width = $view.data('width');
+			height = $view.data('width') / ratio;
+
+		}else if($view.data('height')){
+			height = $view.data('height');
+			width = ratio * $view.data('height');
 		}else{
 			$slider.width('100%');
-			var slide_height = $view_prev.height();
-			var slide_width = $view_prev.width();
+			height = $view.height();
+			width = $view.width();
 		}
-		
+		return {'height' : height, 'width' : width};
+	}
 
+	/* Params
+	/	$slider
+	/	new height of slider
+	/	new width of slider
+	*/
+	function animate_slider($slider, height, width){
 		$slider.animate({
-			height : slide_height+border_height*2,
-			width : slide_width+border_height*2
+			height : height+border_height*2,
+			width : width+border_height*2
 		})
+	}
 
-		$('.slider .view').show();
-		$view.hide();
-		$view.removeClass('view');
-	});
+	/* Params
+	/	$slider
+	/	$curent_view
+	*/
+	function change_slide($slider, $curent_view){
+		$slider.children('.view').show();
+		$curent_view.hide();
+		$curent_view.removeClass('view');
+	}
 });
